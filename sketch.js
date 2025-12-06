b88d9abc4f13641b8355035f0e4b2654c0a436ef
Mahak Gupta <129975137+codedbymahak@users.noreply.github.com>
2025-12-06 20:27:54 +0530

    Update start speed sketch.js

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup() {
   x = random(ballSize/2, width - ballSize/2);
   y = random(ballSize/2, height - ballSize/2);
 
-  xspeed = random(-5, 5);
-  yspeed = random(-5, 5);
+  xspeed = random(0, 5);
+  yspeed = random(0, 5);
 }
 
 function preload() {
